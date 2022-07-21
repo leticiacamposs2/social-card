aa5deda43285f8ab5cb70ab0df02083be8276e71
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-07-21 00:27:38 -0300

    feat: transform the application into a web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,13 @@ import { CardComponent } from './card/card.component';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define('mf-social-card', el);
+  }
+
+  ngDoBootstrap() {}
+}
